e160b8257e978fc1a6b9bb35c7f000afae086d4f
Wjz6 <993620249@qq.com>
2023-07-03 15:32:53 +0800

    购物车功能完成

---
+++ src/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -0,0 +1,18 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 购物车
+ */
+@Slf4j
+@RestController
+@RequestMapping("/shoppingCart")
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+}
